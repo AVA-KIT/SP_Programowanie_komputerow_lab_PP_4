47292c72e94164532756b3fae036942e516a0f56
AVA-KIT <gazda.edu@gmail.com>
2020-02-05 13:24:52 +0100

    Zadanie 7

--- lab4.7.py
+++ lab4.7.py
@@ -1,15 +1,16 @@
-email = 'ala@ala.pl'
-
+email = input('Podaj adres email: ')
+k = ['.pl','.com','.org','.edu','.net']
 def czyEmail(email):
-    k = ['.pl','.com','.org','.edu',]
     for i in k:
-        if email.endswith(i) and '@' in email:
-           return True
-           print ('tak')
+        if email.endswith(i) == False and i != k[-1]:
+           continue
+        elif email.endswith(i) == True and '@' in email and email.index('@') != 0:
+           print ('Twój email jest poprawny')
+           break
         else:
-           return False
-            
-        
+            print ('To nie jest adres email')
+            break
+         
 czyEmail(email)
 
 
