77890ceeb3985bc4ba85d1fda0f36fb208b426ac
AVA-KIT <gazda.edu@gmail.com>
2020-02-11 17:28:38 +0100

    Zadanie 10

--- lab4.10.py
+++ lab4.10.py
@@ -3,5 +3,10 @@ print("Losujemy 5 liczb z podanego zakresu")
 a = float(input('Podaj początek zakresu: ')) 
 b = float(input('Podaj koniec zakresu: '))
 i = 0
+w =[]
+
 for i in range(0,5,1):
-    print(random.uniform(a,b))
+    w.append(round(random.uniform(a,b),3))
+
+for j in w:
+    print(j)
